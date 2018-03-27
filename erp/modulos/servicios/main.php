ded6545b8cd6c789c1c25d97e791d18b3dda3997
Luis Gallego Quero <lgaq94@gmail.com>
2018-03-27 19:31:13 +0200

    Adaptado formulario nuevoServicio

--- erp/modulos/servicios/main.php
+++ erp/modulos/servicios/main.php
@@ -45,13 +45,13 @@
         <?php $op = $_GET['op'];
 
         if($op == "nuevoServicio") include "./nuevoServicio.php";       // DIFUNTOS
-        else if($op == "defunciones") include "./defunciones.php";      // listado
-        else if($op == "v_defuncion") include "./v_defuncion.php";      // ver
-        else if($op == "e_defuncion") include "./e_defuncion.php";      // editar
+        else if($op == "defunciones") include "./defunciones.php";          // listado
+        else if($op == "v_defuncion") include "./v_defuncion.php";          // ver
+        else if($op == "e_defuncion") include "./e_defuncion.php";          // editar
         else if($op == "nuevoCliente") include "./nuevoCliente.php";    // CLIENTES
-        else if($op == "clientes") include "./clientes.php";            // listado
-        else if($op == "v_cliente") include "./v_cliente.php";          // ver
-        else if($op == "e_cliente") include "./e_cliente.php";          // editar
+        else if($op == "clientes") include "./clientes.php";                // listado
+        else if($op == "v_cliente") include "./v_cliente.php";              // ver
+        else if($op == "e_cliente") include "./e_cliente.php";              // editar
 
         ?>
     </div> <!-- fin page-wrapper -->
