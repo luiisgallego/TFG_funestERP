73b063796837e401e681fd825e910809098e0535
Luis Gallego Quero <lgaq94@gmail.com>
2018-04-25 11:59:36 +0200

    Elimino segunda tabla para evitar duplicado de datos provisionalmente

--- erp/modulos/servicios/nuevoServicio.php
+++ erp/modulos/servicios/nuevoServicio.php
@@ -24,7 +24,7 @@
                                     <label>Nombre</label>
                                     <div class="input-group">
                                         <span class="input-group-addon"></span>
-                                        <input type="text" class="form-control" name="nombre" value="prueba" placeholder=""/>
+                                        <input type="text" class="form-control" name="nombre" placeholder=""/>
                                     </div>
                                 </div>
                                 <div class="col-md-3">
@@ -126,104 +126,7 @@
                         </a>
                     </div> <!-- panel-heading -->
                     <div id="datos_familiares" class="panel-body collapse in">
-                        <ul class="list-group diseño_formulario">
-                            <li class="list-group-item">
-                                <div class="col-md-3">
-                                    <label>Nombre</label>
-                                    <div class="input-group">
-                                        <span class="input-group-addon"></span>
-                                        <input type="text" class="form-control" name="nombre" value="prueba" placeholder=""/>
-                                    </div>
-                                </div>
-                                <div class="col-md-3">
-                                    <label>Apellidos</label>
-                                    <div class="input-group">
-                                        <span class="input-group-addon"></span>
-                                        <input type="text" class="form-control" name="apellidos" placeholder=""/>
-                                    </div>
-                                </div>
-                                <div class="col-md-3">
-                                    <label>DNI</label>
-                                    <div class="input-group">
-                                        <span class="input-group-addon"></span>
-                                        <input type="text" class="form-control" name="DNI" placeholder=""/>
-                                    </div>
-                                </div>
-                                <div class="col-md-3">
-                                    <label>Tipo Servicio</label>
-                                    <div class="input-group">
-                                        <span class="input-group-addon"></span>
-                                        <select class="form-control" name="tipo_servicio" title="tipo servicio">
-                                            <option>Particular</option>
-                                            <option>Compañia</option>
-                                            <option>Recepción</option>
-                                        </select>
-                                    </div>
-                                </div>
-                            </li>
-                            <li class="list-group-item">
-                                <div class="col-md-3">
-                                    <label>Natural de</label>
-                                    <div class="input-group">
-                                        <span class="input-group-addon"></span>
-                                        <input type="text" class="form-control" name="natural_de" placeholder=""/>
-                                    </div>
-                                </div>
-                                <div class="col-md-3">
-                                    <label>Provincia</label>
-                                    <div class="input-group">
-                                        <span class="input-group-addon"></span>
-                                        <input type="text" class="form-control" name="provincia" placeholder=""/>
-                                    </div>
-                                </div>
-                                <div class="col-md-3">
-                                    <label>Calle</label>
-                                    <div class="input-group">
-                                        <span class="input-group-addon"></span>
-                                        <input type="text" class="form-control" name="calle" placeholder=""/>
-                                    </div>
-                                </div>
-                                <div class="col-md-3">
-                                    <label>Número</label>
-                                    <div class="input-group">
-                                        <span class="input-group-addon"></span>
-                                        <input type="text" class="form-control" name="numero" placeholder=""/>
-                                    </div>
-                                </div>
-                            </li>
-                            <li class="list-group-item">
-                                <div class="col-md-3">
-                                    <label>Tanatorio</label>
-                                    <div class="input-group">
-                                        <span class="input-group-addon"></span>
-                                        <select class="form-control" name="tanatorio" title="tanatorio">
-                                            <option>No</option>
-                                            <option>Sala 1</option>
-                                            <option>Sala 2</option>
-                                            <option>Sala 3</option>
-                                        </select>
-                                    </div>
-                                </div>
-                                <div class="col-md-3">
-                                    <label>Fecha nacimiento</label>
-                                    <div class="input-group ">
-                                        <div class="input-group-addon"><!--<span class="glyphicon glyphicon-calendar"></span>--></div>
-                                        <input type="date" class="form-control" name="fecha_nacimiento" placeholder=""/>
-                                    </div>
-                                </div>
-                                <div class="col-md-2">
-                                    <label>Estado civil</label>
-                                    <div class="input-group">
-                                        <span class="input-group-addon"></span>
-                                        <select class="form-control" name="estado_civil" title="estado civil">
-                                            <option>Casado</option>
-                                            <option>Viudo</option>
-                                            <option>Soltero</option>
-                                        </select>
-                                    </div>
-                                </div>
-                            </li>
-                        </ul>
+
                     </div> <!-- #datos_familiares panel-body -->
                 </div> <!-- panel -->
 
